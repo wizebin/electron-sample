b3df705060e43a0194fa21c6191f7a07f0408108
wizebin <wizebin@gmail.com>
2021-02-17 18:25:28 -0500

    Small home copy change

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import moment from 'moment';
 
 export default function Home() {
   return (
-    <div>Home {moment().calendar()}</div>
+    <div style={{ padding: 20, backgroundColor: '#333', color: '#fff', height: '100%' }}>
+      <h1>Title</h1>
+      <p>This is a change</p>
+    </div>
   );
 }
